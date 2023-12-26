d70ec2fbd84745db59f0cfd98710a27215981adc
Nandaraihansukma <nanda20005@mail.unpad.ac.id>
2023-12-26 22:49:32 +0700

    ssr switch to true

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const MapComp = dynamic(() => import('@/components/Map/MapComponent'), { ssr: false });
+const MapComp = dynamic(() => import('@/components/Map/MapComponent'), { ssr: true });
 
 async function Map() {
     return (
