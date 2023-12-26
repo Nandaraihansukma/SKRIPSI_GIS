ba347d14f153b7a9030f37280040bcf33bedc846
Nandaraihansukma <nanda20005@mail.unpad.ac.id>
2023-12-26 22:20:56 +0700

    revalidatePath apiGeoloc

--- src/app/api/geoloc/route.ts
+++ src/app/api/geoloc/route.ts
@@ -1,4 +1,5 @@
 import { getGeoLocs } from "@/lib/prisma/geoloc";
+import { revalidatePath } from "next/cache";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -7,6 +8,7 @@ export async function GET(request: NextRequest) {
         if (error) {
             return Response.json({ message: "internal server error" }, { status: 500 });
         }
+        revalidatePath('/');
         return Response.json({ data: res }, { status: 200 });
     } catch (error) {
         return Response.json({ message: "internal server error" }, { status: 500 });
