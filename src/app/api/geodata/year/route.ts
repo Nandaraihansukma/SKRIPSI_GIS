91d3ce943c901736f0e2dc48c552fe310a989f84
Nandaraihansukma <nanda20005@mail.unpad.ac.id>
2024-02-04 13:11:15 +0700

    update tahun lagi

--- src/app/api/geodata/year/route.ts
+++ src/app/api/geodata/year/route.ts
@@ -1,17 +1,20 @@
 import { getYear } from "@/lib/prisma/geodata";
 import { NextRequest } from "next/server";
-import { revalidatePath } from 'next/cache'
-
+import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
-    try {
-        const { res: data, error: geodataserr } = await getYear();
-        if (geodataserr) {
-            return Response.json({ message: "internal server error" }, { status: 500 });
-        }
-        revalidatePath('/')
-        return Response.json({ data: data }, { status: 200 });
-    } catch (error) {
-        return Response.json({ message: "internal server error" }, { status: 500 });
+  try {
+    const { res: data, error: geodataserr } = await getYear();
+    if (geodataserr) {
+      return Response.json(
+        { message: "internal server error" },
+        { status: 500 }
+      );
     }
+    const path = request.nextUrl.searchParams.get("path");
+    revalidatePath(path ?? "/");
+    return Response.json({ data: data }, { status: 200 });
+  } catch (error) {
+    return Response.json({ message: "internal server error" }, { status: 500 });
+  }
 }
