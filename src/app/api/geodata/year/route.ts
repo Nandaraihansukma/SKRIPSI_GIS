1edf349fe437f6a1b24f0c82cbafa84e4ebd6122
Nandaraihansukma <nanda20005@mail.unpad.ac.id>
2024-02-04 12:20:22 +0700

    opting out caching

--- src/app/api/geodata/year/route.ts
+++ src/app/api/geodata/year/route.ts
@@ -1,5 +1,7 @@
 import { getYear } from "@/lib/prisma/geodata";
 import { NextRequest } from "next/server";
+import { revalidatePath } from 'next/cache'
+
 
 export async function GET(request: NextRequest) {
     try {
@@ -7,6 +9,7 @@ export async function GET(request: NextRequest) {
         if (geodataserr) {
             return Response.json({ message: "internal server error" }, { status: 500 });
         }
+        revalidatePath('/')
         return Response.json({ data: data }, { status: 200 });
     } catch (error) {
         return Response.json({ message: "internal server error" }, { status: 500 });
